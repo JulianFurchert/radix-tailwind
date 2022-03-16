0b27f5a853a87d04bc8778066aad527592b1474d
jf <julian.furchert@raytion.com>
2022-03-16 15:01:56 +0100

    Remove Test Component

--- packages/component-library/src/components/TestComponent.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-type TestComponentProps = {
-  example: string;
-}
-
-export const TestComponent: React.FC<TestComponentProps> = (props) => {
-  return <div>{props.example}</div>
-}
--- packages/component-library/src/index.ts
+++ packages/component-library/src/index.ts
@@ -1,3 +1,2 @@
-export { TestComponent } from './components/TestComponent'
 export * as Accordion from './components/Accordion'
 export * as Slider from './components/Slider'
